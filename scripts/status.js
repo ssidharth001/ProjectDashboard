d6ccf3cbc632f85844b64f28906b959348fc55ff
ssidharth001 <sidharths@qburst.com>
2020-11-11 15:51:45 +0530

    Add status Details dynamically

--- scripts/status.js
+++ scripts/status.js
@@ -6,7 +6,7 @@ let yyyy = Number(today.getFullYear());
 let dateList = []
 
 while(ddlimit <= dateToday){
-	const currentDate = ddlimit + '/' + mm + '/' + yyyy;
+	const currentDate = String(ddlimit).padStart(2, '0') + '/' + String(mm).padStart(2, '0') + '/' + yyyy;
     dateList.push(currentDate);
     ddlimit++
 }
@@ -68,42 +68,41 @@ document.querySelector('.status-submit-btn').addEventListener('click', ()=>{
 })
 
 //---------- Loading status history dynamically----------------
-get()
-const statusContainer = document.querySelector(".status-container");
-statusContainer.innerHTML +=`<div class="status-card">   
-<div class="dates-section">
-    <p><span class="date">11/01/2020</span></p>
-</div>
-<div class="details-section">
-    <div class="details-content">
-        <p><span class="details" id="serialnumber">1</span></p>
-        <p>Name: <span class="details" id="name">Nikhil</span></p>
-        <P>Activity:  <span class="details" id="activity">developer</span></P>
-        <p>Hours:  <span class="details" id="hours">8</span></p>
+const getCurrentProjectId = document.querySelector('.selection').dataset.projectid
+const CurrentProjectName = projects.projectList.filter((project)=>project.projectId == getCurrentProjectId)[0].projectName
+const statusDates = [...new Set(statusDetails.map((e)=>e.date))]
+const sortedstatusDates = statusDates.sort((a,b) => a < b ? 1 : -1);
+const currentDateDetails = {}
 
-    </div>
-    <div class="details-content">
-        <p><span class="details" id="serialnumber">1</span></p>
-        <p>Name: <span class="details" id="name">Nikhil</span></p>
-        <P>Activity:  <span class="details" id="activity">developer</span></P>
-        <p>Hours:  <span class="details" id="hours">8</span></p>
+sortedstatusDates.forEach((e)=>{
+    currentDateDetails[e] = statusDetails.filter((d)=> d.date == e)
+})
 
-    </div>
-    <div class="details-content">
-        <p><span class="details" id="serialnumber">1</span></p>
-        <p>Name: <span class="details" id="name">Nikhil</span></p>
-        <P>Activity:  <span class="details" id="activity">developer</span></P>
-        <p>Hours:  <span class="details" id="hours">8</span></p>
+let i = 0 ;  
+for(const details in currentDateDetails){
+    console.log(details)
+    const statusContainer = document.querySelector(".status-container");
+    statusContainer.innerHTML +=
+    `<div class="status-card">   
+        <div class="dates-section">
+            <p><span class="date">${ details }</span></p>
+        </div>
+        <div class="details-section">
+        </div>
+    </div>`;
+ 
+    for(const resources of currentDateDetails[details]){
+        document.querySelectorAll('.details-section')[i].innerHTML += 
+        `<div class="details-content">
+            <p><span class="details" id="serialnumber">1</span></p>
+            <p>Name: <span class="details" id="name">${resources.resourceName}</span></p>
+            <P>Activity:  <span class="details" id="activity">${resources.activityType}</span></P>
+            <p>Hours:  <span class="details" id="hours">${resources.workHours}</span></p>
+        </div>` 
+        console.log(resources)
+        
+    }
+    i++
+}
 
-    </div>
-    <div class="details-content">
-        <p><span class="details" id="serialnumber">1</span></p>
-        <p>Name: <span class="details" id="name">Nikhil</span></p>
-        <P>Activity:  <span class="details" id="activity">developer</span></P>
-        <p>Hours:  <span class="details" id="hours">8</span></p>
 
-    </div>
-    
-
-</div>
-</div>`
